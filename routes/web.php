588da44e47621df8c743231660e6fbdb294eb704
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-08-13 00:39:17 -0300

    Route réservation

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+//Réservation
+Route::prefix("/reservation")->name("reservation")->group(function() {
+    Route::get("/", function () {
+        return view("reservation");
+    });
+});
