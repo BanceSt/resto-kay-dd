7521cf1b1133c27a4b3a10c6af6991c75c3298ee
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-08-13 00:39:49 -0300

    vue accueil et reservation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
-    <title>Acceuil</title>
-</head>
-<body class="bg-green-50">
-<nav class="bg-gray-700 border-gray-200">
-    <div class="w-full flex flex-wrap items-center justify-between p-4">
-        <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl text-gray-100 whitespace-nowrap dark:text-white font-bold">{{str_replace("_", " ", config("app.name"))}}</span>
-        </a>
-        <button class="text-white bg-green-700 text-2xl hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full px-5 py-1.5 text-center me-2 mb-2">
-            RESERVATION
-        </button>
-    </div>
-</nav>
-</body>
-</html>
+@extends("layouts.main")
+
+
+@section("center")
+
+ {{-- Bannière --}}
+@endsection
+
---
+++ resources/views/reservation.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.main")
+
+@section("title", "Réservation")
+@section("center")
+<div class="flex items-center w-7/12 h-screen min-h-screen mx-auto bg-green-100">
+    <div class="w-10/12 bg-red-600 h-5/6 ">
+        test
+    </div>
+</div>
+@endsection
+
