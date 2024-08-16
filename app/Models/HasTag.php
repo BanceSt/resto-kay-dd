4922c9b7affe5e25e656a6888a080ab3e66ce336
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-08-16 00:37:56 -0300

    models et tables Tag, produits, hasTag

---
+++ app/Models/HasTag.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class HasTag extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'produit_id',
+        'tag_id'
+    ];
+    protected $primaryKey = [
+        'produit_id',
+        'tag_id'
+    ];
+
+    public function produit() : BelongsTo
+    {
+        return $this->belongsTo(Produit::class);
+    }
+
+    public function tag() : BelongsTo
+    {
+        return $this->belongsTo(Tag::class);
+    }
+
+
+}
---
+++ app/Models/Produit.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Produit extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "alts",
+        "description",
+        "img",
+        "price",
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'alts' => 'array',
+        ];
+    }
+
+    public function hasTag() : HasMany
+    {
+        return $this->HasMany(HasTag::class);
+    }
+}
